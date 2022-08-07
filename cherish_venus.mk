e0c22bf1acb8c595ae732623eae4218b960b0837
kota <104696918+kota69th@users.noreply.github.com>
2022-08-07 21:16:29 +0200

    introduce the cum

--- cherish_venus.mk
+++ cherish_venus.mk
@@ -1,48 +1,38 @@
-#
-# Copyright (C) 2021-2022 CherishOS.
-# A beautiful ROM based on AOSP and Pixel with good quality of features.
-#
-# Device Name : Xiaomi Mi 11 | Device Codename : venus | Device Model : M2011K2G
-# The Xiaomi Mi 11 was announced in December 2020 and released globally on February 2021
-#
+# bruh
 
-# Inherit from those products. Most specific first.
+# 💨💨💨💨💨💨💨💨💨💨💨💨💨💨💨💨💨
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from venus device.
+# call device
 $(call inherit-product, device/xiaomi/venus/device.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# introduce the cum
+$(call inherit-product, vendor/cum/config/common_full_phone.mk)
 
-# Inherit AnxVerse Setup.
+# meme camera
 $(call inherit-product, vendor/xiaomi/venus-miuicamera/products/miuicamera.mk)
 
-# CherishOS Build TAGS
-CHERISH_BUILD_TYPE=OFFICIAL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=kota
+# your mom
+CUM_FLAG=OFFICIAL
+STEP_DADDY=kota
 
-# Boot Animation Resolution.
-TARGET_BOOT_ANIMATION_RES := 1440
+# your mom's number
+BOOTANIM_CUM := 1440
 
-# Supported Device Flags. (Features)
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-TARGET_SUPPORTS_QUICK_TAP := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_USES_BLUR := true
+# cummy features
+FART_UNLOCK_SUPPORT := true
+CUM_TO_UNLOCK := true
+SUPPORT_ASS_TAP := true
 
 # Google Mobile Services Support. (GAPPS)
 TARGET_GAPPS_ARCH := arm64
 WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := cherish_venus
+PRODUCT_NAME := cum_venus
 PRODUCT_DEVICE := venus
-PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Mi 11
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_MANUFACTURER := Ximi
+PRODUCT_BRAND := Ximi
+PRODUCT_MODEL := Meme 11
+PRODUCT_GMS_CLIENTID_BASE := android-ximi
